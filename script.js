c35066977ef4af637ece80f8d130afc1f6c36eb9
iqbal lazuardi <iqballazuardi12@gmail.com>
2021-06-26 15:58:18 +0700

    initial

---
+++ script.js
@@ -0,0 +1,67 @@
+// var i = 2
+// while ( i <= 20){
+//     console.log(i +  '. i love coding')
+//     i+= 2
+// } 
+
+// var j = 20
+// while (j >= 2){
+//     console.log(j + '. i love coding')
+//     j -= 2
+// }
+
+
+
+// for (var i= 1; i <= 20;  i++){
+//     console.log(i + '. i love coding')
+// }
+
+// for (var i= 20; i >= ;  i--){
+//     console.log(i + '. i will become develover')
+// }
+
+
+// for(i = 1; i <= 4; i++){
+//     for(j = 1; j <=5 ; j++){
+//     document.write('*')
+//     }
+//     document.write('<br>')
+// }
+
+
+
+// for(i = 1; i <=100; i++){
+//     if(i % 2 == 0){
+//         console.log(i +  ' ini bilngan genap')
+//     }
+//     else {
+//         console.log( i +  ' ini bilangan ganjil')
+//     }
+// }
+
+
+for(i = 1; i <=100; i+= 2){
+    if(i % 3 == 0){
+        console.log(i + ' ini kelipatan 3')
+    }
+
+}
+
+
+for(i = 1; i <=100; i+= 5){
+    if(i % 6 == 0){
+        console.log(i + ' ini kelipatan 6')
+    }
+}
+
+for(i = 0; i <=100; i+= 9){
+    if(i % 9 == 0){
+        console.log(i + ' ini kelipatan 9')
+    }
+}
+
+
+
+
+
+
